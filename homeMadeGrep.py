d2e03ae40200efb6def29f2ce9f763fbea3c3ca5
Adriana Sperlea <adriana.sperlea@gmail.com>
2015-04-07 11:28:29 -0700

    Updated homeMadeGrep.py

--- homeMadeGrep.py
+++ homeMadeGrep.py
@@ -3,7 +3,8 @@ homeMadeGrep.py maps a set of DNA sequences from an input file to another input 
 a home made implementation of grep, adapted to the details of our analysis, but can be easily customized for other uses.
 Usage: python homeMadeGrep.py <file1> <file2>
        <file1>: a file containing a list of DNA sequences, one per line
-       <file2>: a file containing the reference chip; every line has the Ensembl ID of the sequence, followed by the sequence
+       <file2>: a file containing the reference chip; every line has the Ensembl ID of the sequence, while the next
+                4 lines contain the sequence
 
 This script assumes that the sequences in the file2 reference are contained between the GGCGCGCC and TTAATTAA primer
 sites, and that the sequences in file1 contain the TTAATTAA primer as well.
@@ -26,9 +27,17 @@ def main():
 
     # hash text
     textHash = {}
-    for line in text:
-        ID = line.split()[0]
-        sequence = line.split()[1][15:-15].upper() #15:-15 slicing removes primers
+    while True:
+        next_4_lines = list(islice(text, 4))
+        if not next_4_lines:
+            break
+
+        header = next_4_lines[0].split()
+        mut = "nat"
+        if len(header) > 5:
+            mut = header[6][:-1]
+        ID = header[0] + "-" + mut
+        sequence = next_4_lines[1].strip().upper() + next_4_lines[2].strip().upper() + next_4_lines[3].strip().upper()
         textHash[sequence] = ID
         
     for line in words:
